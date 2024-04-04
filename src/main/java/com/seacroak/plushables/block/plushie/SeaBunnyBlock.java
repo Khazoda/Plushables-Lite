84337877050596563fd1953e1a957ded9c1e245c
Khazoda <khazoda@gmail.com>
2024-04-04 19:13:55 +0100

    new sea bunny recipe, lang file, shape etc.

--- src/main/java/com/seacroak/plushables/block/plushie/SeaBunnyBlock.java
+++ src/main/java/com/seacroak/plushables/block/plushie/SeaBunnyBlock.java
@@ -10,9 +10,9 @@ public class SeaBunnyBlock extends BasePlushable {
   @Override
   public VoxelShape getShape() {
     VoxelShape shape = VoxelShapes.empty();
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0, 0.375, 0.625, 0.25, 0.625));
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.375, 0.25, 0.375, 0.4375, 0.375, 0.4375));
-    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.5625, 0.25, 0.375, 0.625, 0.375, 0.4375));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0, 0.25, 0.75, 0.5, 0.75));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0.5, 0.25, 0.375, 0.75, 0.375));
+    shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.625, 0.5, 0.25, 0.75, 0.75, 0.375));
 
     return shape;
   }
