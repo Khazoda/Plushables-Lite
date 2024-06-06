22c4bd6362b41e9858a46dd1e9f43893c66da958
Khazoda <khazoda@gmail.com>
2024-06-06 23:08:49 +0100

    small fixes

--- src/main/java/com/seacroak/plushables/block/BuilderBlock.java
+++ src/main/java/com/seacroak/plushables/block/BuilderBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -30,7 +29,7 @@ public class BuilderBlock extends HorizontalFacingBlock {
 
   @Override
   public ActionResult onUse(BlockState state, World world, BlockPos pos,
-                            PlayerEntity player, Hand hand, BlockHitResult hit) {
+                            PlayerEntity player, BlockHitResult hit) {
     if (world.isClient) {
       player.sendMessage(Text.translatable("block." + PlushablesMod.MOD_ID + ".builder.broken"));
     }
