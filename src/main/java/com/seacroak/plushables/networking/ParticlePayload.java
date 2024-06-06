4034c4a7b6f2b72be37642da277963f99bf00b97
Khazoda <khazoda@gmail.com>
2024-06-06 23:07:10 +0100

    Packet bufs -> payloads with codecs

---
+++ src/main/java/com/seacroak/plushables/networking/ParticlePayload.java
@@ -0,0 +1,66 @@
+package com.seacroak.plushables.networking;
+
+import com.seacroak.plushables.util.GenericUtils;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.UUID;
+
+public record ParticlePayload(UUID playerUUID, Vec3d pos, String particleIdentifier, int particleCount, Vec3d offset,
+                              float spread) implements CustomPayload {
+    public static final CustomPayload.Id<ParticlePayload> ID = new CustomPayload.Id<>(GenericUtils.ID("plushable_particle_packet"));
+    public static final PacketCodec<RegistryByteBuf, ParticlePayload> CODEC = PacketCodec.of(ParticlePayload::write, ParticlePayload::read);
+
+
+    public ParticlePayload(PlayerEntity player, BlockPos pos, String particleIdentifier, int particleCount, Vec3d offset, float spread) {
+        this(player.getUuid(), pos.toCenterPos(), particleIdentifier, particleCount, offset, spread);
+    }
+
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+
+    public static void sendParticlePacketToClients(ServerWorld world, ParticlePayload payload) {
+        BlockPos builderPos = new BlockPos((int) payload.pos.x, (int) payload.pos.y, (int) payload.pos.z);
+        /* Iterate through players that can see particle event emitter */
+        PlayerLookup.tracking(world, builderPos).forEach(player -> {
+            if (player.getUuid() == payload.playerUUID)
+                return;
+            ServerPlayNetworking.send(player, new ParticlePayload(payload.playerUUID, payload.pos, payload.particleIdentifier, payload.particleCount, payload.offset, payload.spread));
+        });
+    }
+
+    public void write(PacketByteBuf buf) {
+        buf.writeUuid(playerUUID);
+        buf.writeDouble(pos.x);
+        buf.writeDouble(pos.y);
+        buf.writeDouble(pos.z);
+        buf.writeString(particleIdentifier);
+        buf.writeInt(particleCount);
+        buf.writeDouble(offset.x);
+        buf.writeDouble(offset.y);
+        buf.writeDouble(offset.z);
+        buf.writeFloat(spread);
+    }
+
+    public static ParticlePayload read(RegistryByteBuf buf) {
+        UUID player = buf.readUuid();
+        Vec3d pos = new Vec3d(buf.readDouble(), buf.readDouble(), buf.readDouble());
+        String particleIdentifier = buf.readString();
+        int particleCount = buf.readInt();
+        Vec3d offset = new Vec3d(buf.readDouble(), buf.readDouble(), buf.readDouble());
+        float spread = buf.readFloat();
+
+        return new ParticlePayload(player, pos, particleIdentifier, particleCount, offset, spread);
+    }
+}
---
+++ src/main/java/com/seacroak/plushables/networking/SoundPayloadPlayerless.java
@@ -0,0 +1,66 @@
+package com.seacroak.plushables.networking;
+
+import com.seacroak.plushables.util.GenericUtils;
+import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+public record SoundPayloadPlayerless(Vec3d pos, String soundIdentifier,
+                                     float pitch) implements CustomPayload {
+    public static final CustomPayload.Id<SoundPayloadPlayerless> ID = new CustomPayload.Id<>(GenericUtils.ID("plushable_sound_packet_without_player"));
+    public static final PacketCodec<RegistryByteBuf, SoundPayloadPlayerless> CODEC = PacketCodec.of(SoundPayloadPlayerless::write, SoundPayloadPlayerless::read);
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+
+    public static void sendNoPlayerPacketToClients(ServerWorld world, BlockPos pos,  String soundIdentifier, float pitch) {
+        BlockPos builderPos = new BlockPos((int) pos.getX(), (int) pos.getY(), (int) pos.getZ());
+        /* Iterate through players that can see sound event emitter */
+        PlayerLookup.tracking(world, builderPos).forEach(player -> {
+            ServerPlayNetworking.send(player, new SoundPayloadPlayerless(pos.toCenterPos(),soundIdentifier, pitch));
+        });
+    }
+
+    public void write(PacketByteBuf buf) {
+        buf.writeDouble(pos.getX());
+        buf.writeDouble(pos.getY());
+        buf.writeDouble(pos.getZ());
+        buf.writeString(soundIdentifier);
+        buf.writeFloat(pitch);
+    }
+
+    public static SoundPayloadPlayerless read(PacketByteBuf buf) {
+        Vec3d pos = new Vec3d(buf.readDouble(), buf.readDouble(), buf.readDouble());
+        String soundIdentifier = buf.readString();
+        float pitch = buf.readFloat();
+        return new SoundPayloadPlayerless(pos, soundIdentifier, pitch);
+    }
+
+
+//    /* Packet originating from playerUUID */
+//    public NoPlayerSoundPacket(UUID playerUUID, Vec3d pos, String soundIdentifier, float pitch) {
+//        this.pos = pos;
+//        this.soundIdentifier = soundIdentifier;
+//        this.pitch = pitch;
+//    }
+//
+//    /* Packet originating from server */
+//    public NoPlayerSoundPacket(Vec3d pos, String soundIdentifier, float pitch) {
+//        this.pos = pos;
+//        this.soundIdentifier = soundIdentifier;
+//        this.pitch = pitch;
+//    }
+//
+//    public NoPlayerSoundPacket(BlockPos pos, SoundEvent soundEvent, float pitch) {
+//        this(pos.toCenterPos(), soundEvent.getId().toString(), pitch);
+//    }
+
+}
