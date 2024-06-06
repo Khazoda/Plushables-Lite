c796bd967f593fe3b74315fa024d74dac7366bb2
Khazoda <khazoda@gmail.com>
2024-06-06 23:08:57 +0100

    attempt to port armor item

--- src/main/java/com/seacroak/plushables/item/CapArmorItem.java
+++ src/main/java/com/seacroak/plushables/item/CapArmorItem.java
@@ -1,65 +1,47 @@
 package com.seacroak.plushables.item;
 
 
-import com.google.common.collect.Multimap;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.item.*;
-
-
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.EnvType;
+import com.seacroak.plushables.PlushablesMod;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.sound.SoundEvent;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.EnumMap;
+import java.util.List;
+
+import static com.seacroak.plushables.registry.MainRegistry.CAP_MATERIAL;
 
 public class CapArmorItem extends ArmorItem {
-  static final Item.Settings capItemSettings = new Item.Settings().maxCount(1);
+    static final Item.Settings capItemSettings = new Item.Settings().maxCount(1);
 
-  public CapArmorItem(String name) {
-    super(new ArmorMaterial() {
-      @Override
-      public int getDurability(Type type) {
-        return 10;
-      }
+    public static final ArmorMaterial material = new ArmorMaterial(
+            Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+                map.put(ArmorItem.Type.HELMET, 2);
+                map.put(Type.CHESTPLATE, 2);
+                map.put(Type.LEGGINGS, 2);
+                map.put(Type.BOOTS, 2);
 
-      @Override
-      public int getProtection(Type type) {
-        return 1;
-      }
+            }), 20,
+            SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, () -> Ingredient.ofItems(Items.LEATHER),
+            List.of(
+                    new ArmorMaterial.Layer(
+                            new Identifier(PlushablesMod.MOD_ID, "cap")
+                    ),
+                    new ArmorMaterial.Layer(
+                            new Identifier(PlushablesMod.MOD_ID, "cap"), "_froglin" + "_overlay", false
+                    )
+            ),
+            0, 0
+    );
 
-      @Override
-      public int getEnchantability() {
-        return 0;
-      }
 
-      @Override
-      public SoundEvent getEquipSound() {
-        return null;
-      }
-
-      @Override
-      public Ingredient getRepairIngredient() {
-        return Ingredient.ofStacks(new ItemStack(Items.LEATHER));
-      }
-
-      @Environment(EnvType.CLIENT)
-      @Override
-      public String getName() {
-        return name;
-      }
-
-      @Override
-      public float getToughness() {
-        return 0.5f;
-      }
-
-      @Override
-      public float getKnockbackResistance() {
-        return 0f;
-      }
-    }, Type.HELMET, capItemSettings);
-  }
+    public CapArmorItem(String name) {
+        super(RegistryEntry.of(CAP_MATERIAL), Type.HELMET, capItemSettings);
+    }
 }
