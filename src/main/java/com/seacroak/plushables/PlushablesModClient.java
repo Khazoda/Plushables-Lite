1c65d98a4dfe1ffbbd9be21d0c841987dfe55188
Khazoda <khazoda@gmail.com>
2024-06-06 23:07:58 +0100

    networking

--- src/main/java/com/seacroak/plushables/PlushablesModClient.java
+++ src/main/java/com/seacroak/plushables/PlushablesModClient.java
@@ -1,12 +1,6 @@
 package com.seacroak.plushables;
 
-import com.seacroak.plushables.config.ConfigPacketHandler;
-import com.seacroak.plushables.config.ConfigPacketHandler.ConfigPacket;
 import com.seacroak.plushables.networking.*;
-import com.seacroak.plushables.networking.AnimationPacketHandler.AnimationPacket;
-import com.seacroak.plushables.networking.ParticlePacketHandler.ParticlePacket;
-import com.seacroak.plushables.networking.SoundPacketHandler.NoPlayerSoundPacket;
-import com.seacroak.plushables.networking.SoundPacketHandler.PlayerSoundPacket;
 import com.seacroak.plushables.registry.MainRegistry;
 import com.seacroak.plushables.registry.client.EntityRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
@@ -16,6 +10,7 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.sound.SoundEvent;
@@ -24,132 +19,112 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Position;
 
 public final class PlushablesModClient implements ClientModInitializer {
-  public static boolean onServer = false;
+    public static boolean onServer = false;
 
-  @Override
-  @Environment(EnvType.CLIENT)
-  public void onInitializeClient() {
-    EntityRendererRegistry.initClient();
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void onInitializeClient() {
+        PayloadTypeRegistry.playC2S().register(ConfigSyncPayload.ID, ConfigSyncPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(ParticlePayload.ID, ParticlePayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(SoundPayload.ID, SoundPayload.CODEC);
+        PayloadTypeRegistry.playS2C().register(SoundPayloadPlayerless.ID, SoundPayloadPlayerless.CODEC);
+        EntityRendererRegistry.initClient();
 
-    /* Functional Transparency*/
-    /* Blocks */
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BASKET_BLOCK, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BUILDER_BLOCK, RenderLayer.getCutout());
+        /* Functional Transparency*/
+        /* Blocks */
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BASKET_BLOCK, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BUILDER_BLOCK, RenderLayer.getCutout());
 
-    /* Simple Plushables */
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.PIG_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TRUFFLES_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.WHELPLING_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.RAPTOR_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BIG_TATER_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BIG_IRRITATER_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.OTTER_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.SHRUMP_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.WHALE_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.GOLDFISH_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TRATER_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.FROGE_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.MAMMOTH_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TIGER_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BLAHAJ_PLUSHABLE, RenderLayer.getCutout());
+        /* Simple Plushables */
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.PIG_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TRUFFLES_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.WHELPLING_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.RAPTOR_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BIG_TATER_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BIG_IRRITATER_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.OTTER_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.SHRUMP_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.WHALE_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.GOLDFISH_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TRATER_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.FROGE_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.MAMMOTH_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.TIGER_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.BLAHAJ_PLUSHABLE, RenderLayer.getCutout());
 
-    /* Complex Plushables */
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.RUPERT_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.OWL_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.STATUETTE_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.CLUCKY_PLUSHABLE, RenderLayer.getCutout());
-    BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.DRAGON_PLUSHABLE, RenderLayer.getCutout());
+        /* Complex Plushables */
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.RUPERT_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.OWL_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.STATUETTE_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.CLUCKY_PLUSHABLE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(MainRegistry.DRAGON_PLUSHABLE, RenderLayer.getCutout());
 
-    /* Clientside Commands */
-    ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess)
-            -> dispatcher.register(ClientCommandManager.literal("plushables")
-            .executes(context -> {
-                  context.getSource().sendFeedback(Text.translatable("command.plushables.root"));
-                  return 1;
-                }
-            )
-            .then(ClientCommandManager.literal("wiki")
-                .executes(context -> {
-                  context.getSource().sendFeedback(Text.translatable("command.plushables.wiki").setStyle(Style.EMPTY.withColor(Formatting.BLUE).withUnderline(true).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,"https://plushables.khazoda.com"))));
-                  return 1;
-                })
-            )
-        )
-    );
+        /* Clientside Commands */
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess)
+                        -> dispatcher.register(ClientCommandManager.literal("plushables")
+                        .executes(context -> {
+                                    context.getSource().sendFeedback(Text.translatable("command.plushables.root"));
+                                    return 1;
+                                }
+                        )
+                        .then(ClientCommandManager.literal("wiki")
+                                .executes(context -> {
+                                    context.getSource().sendFeedback(Text.translatable("command.plushables.wiki").setStyle(Style.EMPTY.withColor(Formatting.BLUE).withUnderline(true).withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://plushables.khazoda.com"))));
+                                    return 1;
+                                })
+                        )
+                )
+        );
 
-    /* Config Sync Networking Packet Client Receipt */
-    ClientPlayNetworking.registerGlobalReceiver(ConfigPacketHandler.PACKET_ID, ((client, handler, buf, responseSender) -> {
-      var packet = ConfigPacket.read(buf);
-      if (client == null) return;
-      client.execute(() -> {
-        PlushablesNetworking.priorityConfig(packet.enable_basket, packet.allow_all_block_items_in_baskets);
-      });
-    }));
+        /* Config Sync Networking Packet Client Receipt */
+        ClientPlayNetworking.registerGlobalReceiver(ConfigSyncPayload.ID, (payload, context) -> {
+            if (context.client().world == null) return;
+            context.client().execute(() -> {
+                PlushablesNetworking.priorityConfig(payload.enableBasket(), payload.allowAllBlockItems());
+            });
+        });
 
-    /* Sound Event Networking Packet Client Receipt */
-    ClientPlayNetworking.registerGlobalReceiver(SoundPacketHandler.PACKET_ID_PLAYER, ((client, handler, buf, responseSender) -> {
-      var packet = PlayerSoundPacket.read(buf);
-      SoundEvent decodedSoundEvent = PacketDecoder.decodeSoundEvent(packet.soundIdentifier);
-      if (client == null) return;
+        /* Sound Event Networking Packet Client Receipt */
+        ClientPlayNetworking.registerGlobalReceiver(SoundPayload.ID, (payload, context) -> {
+            SoundEvent decodedSoundEvent = PacketDecoder.decodeSoundEvent(payload.soundIdentifier());
+            if (context.client() == null) return;
 
-      assert client.player != null;
-      if (packet.player == client.player.getUuid())
-        return;
-      client.execute(() -> {
-        if (client.world == null)
-          return;
-        PlushablesNetworking.playSoundOnClient(decodedSoundEvent, client.world, BlockPos.ofFloored(packet.pos), 1f, packet.pitch);
+            assert context.client().player != null;
+            if (payload.playerUUID() == context.client().player.getUuid())
+                return;
+            context.client().execute(() -> {
+                if (context.client().world == null)
+                    return;
+                PlushablesNetworking.playSoundOnClient(decodedSoundEvent, context.client().world, BlockPos.ofFloored(payload.pos()), 1f, payload.pitch());
+            });
+        });
 
-      });
-    }));
+        /* Sound Event Networking Packet Client Receipt */
+        ClientPlayNetworking.registerGlobalReceiver(SoundPayloadPlayerless.ID, (payload, context) -> {
+            SoundEvent decodedSoundEvent = PacketDecoder.decodeSoundEvent(payload.soundIdentifier());
+            if (context.client() == null) return;
+            context.client().execute(() -> {
+                if (context.client().world == null)
+                    return;
+                PlushablesNetworking.playSoundOnClient(decodedSoundEvent, context.client().world, BlockPos.ofFloored((Position) payload.pos()), 1f, payload.pitch());
+            });
+        });
 
-    /* Sound Event Networking Packet Client Receipt */
-    ClientPlayNetworking.registerGlobalReceiver(SoundPacketHandler.PACKET_ID_NO_PLAYER, ((client, handler, buf, responseSender) -> {
-      var packet = NoPlayerSoundPacket.read(buf);
-      SoundEvent decodedSoundEvent = PacketDecoder.decodeSoundEvent(packet.soundIdentifier);
-      if (client == null) return;
-      client.execute(() -> {
-        if (client.world == null)
-          return;
-        PlushablesNetworking.playSoundOnClient(decodedSoundEvent, client.world, BlockPos.ofFloored(packet.pos), 1f, packet.pitch);
-      });
-    }));
-
-    /* Particle Networking Packet Client Receipt */
-    ClientPlayNetworking.registerGlobalReceiver(ParticlePacketHandler.PACKET_ID, ((client, handler, buf, responseSender) -> {
-      var packet = ParticlePacket.read(buf);
-      ParticleEffect decodedParticles = PacketDecoder.decodeParticle(packet.particleIdentifier);
-      if (client == null) return;
-
-      assert client.player != null;
-      if (packet.player == client.player.getUuid())
-        return;
-      client.execute(() -> {
-        if (client.world == null)
-          return;
-        PlushablesNetworking.spawnParticlesOnClient(decodedParticles, client.world, BlockPos.ofFloored(packet.pos), packet.particleCount, packet.offset, packet.spread);
-
-      });
-    }));
-
-    /* Animation Event Networking Packet Client Receipt */
-    ClientPlayNetworking.registerGlobalReceiver(AnimationPacketHandler.PACKET_ID, ((client, handler, buf, responseSender) -> {
-      var packet = AnimationPacket.read(buf);
-      if (client == null) return;
-
-      assert client.player != null;
-      if (packet.player == client.player.getUuid())
-        return;
-      client.execute(() -> {
-        if (client.world == null)
-          return;
-        PlushablesNetworking.playAnimationOnClient(packet.shouldAnimate, client.world, BlockPos.ofFloored(packet.pos), packet.animationName);
-
-      });
-    }));
-
-
-  }
+        /* Particle Networking Packet Client Receipt */
+        ClientPlayNetworking.registerGlobalReceiver(ParticlePayload.ID, (payload, context) -> {
+            ParticleEffect decodedParticles = PacketDecoder.decodeParticle(payload.particleIdentifier());
+            if (context.client() == null) return;
+            assert context.client().player != null;
+            if (payload.playerUUID() == context.client().player.getUuid())
+                return;
+            context.client().execute(() -> {
+                if (context.client().world == null)
+                    return;
+                PlushablesNetworking.spawnParticlesOnClient(decodedParticles, context.client().world, BlockPos.ofFloored(payload.pos()), payload.particleCount(), payload.offset(), payload.spread());
+            });
+        });
+    }
 }
--- src/main/java/com/seacroak/plushables/networking/PlushablesNetworking.java
+++ src/main/java/com/seacroak/plushables/networking/PlushablesNetworking.java
@@ -1,14 +1,8 @@
 package com.seacroak.plushables.networking;
 
 import com.seacroak.plushables.config.ClientConfigValues;
-import com.seacroak.plushables.config.ConfigPacketHandler;
-import com.seacroak.plushables.networking.AnimationPacketHandler.AnimationPacket;
-import com.seacroak.plushables.networking.ParticlePacketHandler.ParticlePacket;
-import com.seacroak.plushables.networking.SoundPacketHandler.NoPlayerSoundPacket;
-import com.seacroak.plushables.networking.SoundPacketHandler.PlayerSoundPacket;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
@@ -17,6 +11,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
+import java.util.Objects;
 import java.util.Random;
 
 import static com.seacroak.plushables.config.PlushablesConfig.allow_all_block_items_in_baskets;
@@ -24,102 +19,77 @@ import static com.seacroak.plushables.config.PlushablesConfig.enable_baskets;
 
 public class PlushablesNetworking {
 
-  /* This method overwrites the priority of the client's config settings to use the server's instead */
-  public static void priorityConfig(boolean enableBaskets, boolean allowAllBlockItems) {
-    ClientConfigValues.enable_baskets = enableBaskets;
-    ClientConfigValues.allow_all_block_items_in_baskets = allowAllBlockItems;
-  }
-
-  public static void registerServersideClientJoinListener() {
-    ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-      ServerPlayerEntity joinedPlayer = handler.getPlayer();
-      ConfigPacketHandler.sendConfigPacketToClient(joinedPlayer, new ConfigPacketHandler.ConfigPacket(joinedPlayer.getUuid(), enable_baskets, allow_all_block_items_in_baskets));
-    });
-  }
-
-  /* Call this method after sending packet when wanting to play sound */
-  public static void playSoundOnClient(SoundEvent sound, World world, BlockPos pos, float volume, float pitch) {
-    try {
-      Vec3d vec = pos.toCenterPos();
-      world.playSoundAtBlockCenter(BlockPos.ofFloored(vec), sound, SoundCategory.BLOCKS, volume, pitch, true);
-    } catch (Exception e) {
-      System.out.println("Caught log-in animation exception");
+    /* This method overwrites the priority of the client's config settings to use the server's instead */
+    public static void priorityConfig(boolean enableBaskets, boolean allowAllBlockItems) {
+        ClientConfigValues.enable_baskets = enableBaskets;
+        ClientConfigValues.allow_all_block_items_in_baskets = allowAllBlockItems;
     }
-  }
 
-  /* Call this method after sending packet when wanting to spawn particles */
-  public static void spawnParticlesOnClient(ParticleEffect particleType, World world, BlockPos pos, int particleCount, Vec3d offset, float spread) {
-    try {
-      Vec3d vec = pos.toCenterPos();
-      Random rand = new Random();
-      for (int i = 0; i < particleCount; i++) {
-        if (spread == 0) world.addParticle(particleType, vec.x + offset.x, vec.y + offset.y, vec.z + offset.x, 0, 0, 0);
-        if (spread != 0) world.addParticle(particleType, vec.x + offset.x, vec.y + offset.y, vec.z + offset.x,
-            rand.nextFloat(-spread, spread), rand.nextFloat(-spread, spread), rand.nextFloat(-spread, spread));
-      }
-    } catch (Exception e) {
-      System.out.println("Caught log-in animation exception");
+    public static void registerServersideClientJoinListener() {
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            ServerPlayerEntity joinedPlayer = handler.getPlayer();
+            ConfigSyncPayload.sendConfigPacketToClient(joinedPlayer, joinedPlayer.getUuid(), enable_baskets, allow_all_block_items_in_baskets);
+        });
     }
-  }
 
-  public static void playAnimationOnClient(boolean shouldAnimate, World world, BlockPos pos, String animationName) {
-    try {
-      Vec3d vec = pos.toCenterPos();
-      BlockEntity blockEntity = world.getBlockEntity(pos);
-//      if (blockEntity instanceof PoweredBlockEntity) {
-//        ((PoweredBlockEntity) blockEntity).animationName = animationName;
-//        ((PoweredBlockEntity) blockEntity).shouldAnimate = shouldAnimate;
-//      }
-    } catch (Exception e) {
-      System.out.println("Caught log-in animation exception");
+    /* Call this method after sending packet when wanting to play sound */
+    public static void playSoundOnClient(SoundEvent sound, World world, BlockPos pos, float volume, float pitch) {
+        try {
+            Vec3d vec = pos.toCenterPos();
+            world.playSoundAtBlockCenter(BlockPos.ofFloored(vec), sound, SoundCategory.BLOCKS, volume, pitch, true);
+        } catch (Exception e) {
+            System.out.println("Caught log-in animation exception");
+        }
     }
-  }
 
-  /* Receiver WITH Player Data*/
-  public static void registerGlobalSoundPacketReceiverWithPlayer() {
-    /* Registers global packet receiver in MainRegistry.class */
-    ServerPlayNetworking.registerGlobalReceiver(SoundPacketHandler.PACKET_ID_PLAYER, ((server, player, handler, buf, responseSender) -> {
-      var packet = PlayerSoundPacket.read(buf);
-      if (packet.player == player.getUuid())
-        return;
-      server.execute(() -> {
-        SoundPacketHandler.sendPlayerPacketToClients(player.getServerWorld(), packet);
-      });
-    }));
-  }
+    /* Call this method after sending packet when wanting to spawn particles */
+    public static void spawnParticlesOnClient(ParticleEffect particleType, World world, BlockPos pos, int particleCount, Vec3d offset, float spread) {
+        try {
+            Vec3d vec = pos.toCenterPos();
+            Random rand = new Random();
+            for (int i = 0; i < particleCount; i++) {
+                if (spread == 0)
+                    world.addParticle(particleType, vec.x + offset.x, vec.y + offset.y, vec.z + offset.x, 0, 0, 0);
+                if (spread != 0) world.addParticle(particleType, vec.x + offset.x, vec.y + offset.y, vec.z + offset.x,
+                        rand.nextFloat(-spread, spread), rand.nextFloat(-spread, spread), rand.nextFloat(-spread, spread));
+            }
+        } catch (Exception e) {
+            System.out.println("Caught log-in animation exception");
+        }
+    }
 
-  /* Receiver WITHOUT Player Data*/
-  public static void registerGlobalSoundPacketReceiverWithoutPlayer() {
-    /* Registers global packet receiver in MainRegistry.class */
-    ServerPlayNetworking.registerGlobalReceiver(SoundPacketHandler.PACKET_ID_NO_PLAYER, ((server, player, handler, buf, responseSender) -> {
-      var packet = NoPlayerSoundPacket.read(buf);
-      server.execute(() -> {
-        SoundPacketHandler.sendNoPlayerPacketToClients(player.getServerWorld(), packet);
-      });
-    }));
-  }
+    /* Receiver WITH Player Data*/
+    public static void registerGlobalSoundPacketReceiverWithPlayer() {
+        /* Registers global packet receiver in MainRegistry.class */
+        ServerPlayNetworking.registerGlobalReceiver(SoundPayload.ID, (payload, context) -> {
+            if (payload.playerUUID() == context.player().getUuid())
+                return;
+            context.player().getServer().execute(() -> {
+                SoundPayload.sendPlayerPacketToClients(context.player().getServerWorld(), new SoundPayload(payload.playerUUID(), payload.pos(), payload.soundIdentifier(), payload.pitch()));
+            });
+        });
+    }
 
-  public static void registerGlobalParticlePacketReceiver() {
-    /* Registers global packet receiver in MainRegistry.class */
-    ServerPlayNetworking.registerGlobalReceiver(ParticlePacketHandler.PACKET_ID, ((server, player, handler, buf, responseSender) -> {
-      var packet = ParticlePacket.read(buf);
-      if (packet.player == player.getUuid())
-        return;
-      server.execute(() -> {
-        ParticlePacketHandler.sendPacketToClients(player.getServerWorld(), packet);
-      });
-    }));
-  }
+    /* Receiver WITHOUT Player Data*/
+    public static void registerGlobalSoundPacketReceiverWithoutPlayer() {
+        /* Registers global packet receiver in MainRegistry.class */
+        ServerPlayNetworking.registerGlobalReceiver(SoundPayloadPlayerless.ID, (payload, context) -> {
+            context.player().getServer().execute(() -> {
+                SoundPayloadPlayerless.sendNoPlayerPacketToClients(context.player().getServerWorld(), BlockPos.ofFloored(payload.pos()), payload.soundIdentifier(), payload.pitch());
+            });
+        });
+    }
 
-  public static void registerGlobalAnimationPacketReceiver() {
-    /* Registers global packet receiver in MainRegistry.class */
-    ServerPlayNetworking.registerGlobalReceiver(AnimationPacketHandler.PACKET_ID, ((server, player, handler, buf, responseSender) -> {
-      var packet = AnimationPacket.read(buf);
-      if (packet.player == player.getUuid())
-        return;
-      server.execute(() -> {
-        AnimationPacketHandler.sendPacketToClients(player.getServerWorld(), packet);
-      });
-    }));
-  }
+    public static void registerGlobalParticlePacketReceiver() {
+        /* Registers global packet receiver in MainRegistry.class */
+        ServerPlayNetworking.registerGlobalReceiver(ParticlePayload.ID, (payload, context) -> {
+            if (payload.playerUUID() == context.player().getUuid())
+                return;
+            Objects.requireNonNull(context.player().getServer()).execute(() -> {
+                ParticlePayload.sendParticlePacketToClients
+                        (context.player().getServerWorld(), new ParticlePayload(payload.playerUUID(), payload.pos(), payload.particleIdentifier(), payload.particleCount(), payload.offset(), payload.spread()));
+            });
+        });
+    }
+
 }
